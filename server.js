2462d5a1a78a32f0df9eba87e261b476345e08f9
MartyV33 <martinverdoza33@gmail.com>
2024-08-30 20:07:41 -0500

    Creating home.ejs

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-
+app.set('view engine', 'esj');
 
 app.get('/', (req, res) => {
-  res.send('Hello There!');
+  res.render('home');
 });
 
 app.listen(3000);
